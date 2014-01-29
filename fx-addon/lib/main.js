83c9c90ca9c0c450f1fbd22d84ba1f170783d959
Mark Goodwin <mgoodwin@mozilla.com>
2014-01-29 10:01:25 +0000

    data/inject.js

--- fx-addon/lib/main.js
+++ fx-addon/lib/main.js
@@ -1,13 +1,8 @@
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
 
-var script = data.load("oracle.js");
-
 pageMod.PageMod({
   include: ["http://*","https://*","file:/*"],
   contentScriptFile: data.url("inject.js"),
-  contentScriptWhen: 'start',
-  onAttach: function(worker) {
-    worker.port.emit("inject", script);
-  }
+  contentScriptWhen: 'start'
 });
